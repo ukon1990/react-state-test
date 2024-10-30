771e6e638d12aff93f46eb4446622cd3e24bce11
Ukon1990 <jonas.flones@outlook.com>
2024-10-30 23:19:46 +0100

    ASD

---
+++ zustad/src/pages/Movie.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import {Card, CardContent, CardHeader} from "@mui/material";
+
+export const Movie = () => {
+    const [movie, setMovie] = useState({});
+
+    return (
+        <Card>
+            <CardHeader>asd</CardHeader>
+            <CardContent></CardContent>
+        </Card>
+    );
+};
